1b2c784a64e9de89f5d4d81a10f623e3d71e399c
gkumara <geetha.kumarasamy@centurylink.com>
2021-02-17 04:36:04 +0000

    Final Project

--- main.js
+++ main.js
@@ -159,8 +159,8 @@ class ServiceNowAdapter extends EventEmitter {
     this.connector.post( (data, error) => {
             if (data.has('body')) {
             const bodyObj = JSON.parse(data['body']);
-            const item = bodyObj['result'];
-            const newResults = item.map(item => {
+            const xyz = bodyObj['result'];
+            const newResults = xyz.map(item => {
             const container = {};
             container['change_ticket_number'] = item['number'];
             container['active'] = item['active'];
